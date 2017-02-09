d624e1043c7d9f946ff3ce4e1b152dbf6a1562f7
Serasar <you@example.com>
2017-02-09 15:43:59 +0300

    Commented out test file.

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
-from os import urandom
-from hashlib import sha256
-
-def create_tickets(number):
-    for i in range(number):
-        temp_number = (sha256(urandom(30)).hexdigest())
-
-create_tickets(10000)
+#from os import urandom
+#from hashlib import sha256
+#
+#def create_tickets(number):
+#    for i in range(number):
+#        temp_number = (sha256(urandom(30)).hexdigest())
+#
+#create_tickets(10000)
